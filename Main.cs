76620b55393adf0e03dc7f7c3a4b3a6a771666c6
Buggem's Fedora PC <66670928+Buggem@users.noreply.github.com>
2025-09-01 16:31:49 +1000

    Add recursive directory scanning

--- Main.cs
+++ Main.cs
@@ -35,7 +35,7 @@ Usage: hltools.exe [infile.wad/.spr] [outfolder]
 PROTIP: you can also use this utility to determine if a
         file without a valid extension is actually
         GoldSrc compatible.";
-        private static bool isWad = false;
+        private static int fileType = -1;
         static void Main(string[] args)
         {
             if (args.Length > 2 || args.Length < 1)
@@ -52,11 +52,12 @@ PROTIP: you can also use this utility to determine if a
                     if (args[0].EndsWith(".spr"))
                     {
                         Console.WriteLine(sprDetect);
+                        fileType = 0;
                     }
                     else if (args[0].EndsWith(".wad"))
                     {
                         Console.WriteLine(wadDetect);
-                        isWad = true;
+                        fileType = 1;
                     }
                     else if (File.Exists(args[i]))
                     {
@@ -71,11 +72,12 @@ PROTIP: you can also use this utility to determine if a
                             if (header == SpriteLoader.SpriteHeaderId)
                             {
                                 Console.WriteLine(sprDetect);
+                                fileType = 0;
                             }
                             else if (header == Encoding.ASCII.GetString(WAD3Loader.WadHeaderId))
                             {
                                 Console.WriteLine(wadDetect);
-                                isWad = true;
+                                fileType = 1;
                             }
                             else
                             {
@@ -90,6 +92,9 @@ PROTIP: you can also use this utility to determine if a
                         {
                             throw new HLToolsUnsupportedFile(e.Message);
                         }
+                    } else if(Directory.Exists(args[i]))
+                    {
+                        fileType = 2; // recursive scan
                     }
                     else
                     {
@@ -98,7 +103,7 @@ PROTIP: you can also use this utility to determine if a
                 }
                 if (i == 1)
                 {
-                    if (isWad)
+                    if (fileType == 1)
                     {
                         WAD3Loader wad3l = new WAD3Loader();
                         Console.WriteLine(string.Concat("Reading WADfile ", Path.GetFileName(args[0]), "..."));
@@ -129,7 +134,7 @@ PROTIP: you can also use this utility to determine if a
                         // close wad reader gracefully
                         wad3l.Close();
                     }
-                    else
+                    else if (fileType == 0)
                     {
                         SpriteLoader sl = new SpriteLoader();
 
@@ -138,11 +143,72 @@ PROTIP: you can also use this utility to determine if a
 
                         for (int j = 0; j < outSpr.Length; j++)
                         {
-                            Console.WriteLine(string.Concat("Extracting ", Path.GetFileName(args[0]), "..."));
+                            Console.WriteLine(string.Concat("Extracting ", Path.GetFileName(args[0]), " frame ", j, "..."));
                             outSpr[j].Image.Save(string.Concat(args[1], "/", Path.GetFileNameWithoutExtension(args[0]), j, ".png"));
                         }
                         // close sprite reader gracefully
                         sl.Close();
+                    }
+                    else if (fileType == 2)
+                    {
+                        string[] sprFiles = Directory.GetFiles(args[0], "*.spr", SearchOption.AllDirectories);
+                        SpriteLoader sl = new SpriteLoader();
+
+                        Console.WriteLine(string.Concat("Recursively scanning ", args[0], " for Sprites..."));
+                        for (int k = 0; k < sprFiles.Length; k++)
+                        {
+                            SpriteLoader.Frame[] outSpr = sl.LoadFile(sprFiles[k]); // transparency will be ignored by default, so omit that parameter
+                            string relativePathTo = Path.GetDirectoryName(Path.GetRelativePath(args[0], sprFiles[k]));
+                            // create directory
+                            Directory.CreateDirectory(string.Concat(args[1], "/", relativePathTo));
+
+                            for (int j = 0; j < outSpr.Length; j++)
+                            {
+                                Console.WriteLine(string.Concat("Extracting ", Path.GetFileName(sprFiles[k]), " frame ", j, "..."));
+                                outSpr[j].Image.Save(string.Concat(args[1], "/", relativePathTo, "/", Path.GetFileNameWithoutExtension(sprFiles[k]), j, ".png"));
+                            }
+                        }
+
+
+                        string[] wadFiles = Directory.GetFiles(args[0], "*.wad", SearchOption.AllDirectories);
+                        WAD3Loader wad3l = new WAD3Loader();
+
+
+                        Console.WriteLine(string.Concat("Recursively scanning ", args[0], " for WADs..."));
+                        for (int k = 0; k < wadFiles.Length; k++)
+                        {
+                            wad3l.LoadFile(wadFiles[k]);
+                            List<WAD3Loader.Texture> textures = new List<WAD3Loader.Texture>();
+                            List<string> textureNames = new List<string>();
+
+                            // loop through WAD lumps, only grab textures (I mean do GoldSource wads store anything else?)
+                            for (int j = 0; j < wad3l.LumpsInfo.Count; j++)
+                            {
+                                byte type = wad3l.LumpsInfo[j].Type;
+
+                                // see WAD3Loader.cs for documentation on what these are
+                                if (type == 0x40 || type == 0x42 || type == 0x43 || type == 0x46)
+                                {
+                                    textures.Add(wad3l.GetLumpImage(j)); // same deal as sprites, omit transparancy param as it's default is good
+                                    textureNames.Add(wad3l.LumpsInfo[j].Name);
+                                }
+                            }
+
+                            // create directory
+                            Directory.CreateDirectory(string.Concat(args[1], "/wads/", Path.GetFileNameWithoutExtension(wadFiles[k])));
+                            Console.WriteLine(string.Concat("Extracting ", Path.GetFileName(wadFiles[k]), "..."));
+
+                            // literal copy-paste from sprites
+                            for (int j = 0; j < textures.Count; j++)
+                            {
+                                Console.WriteLine(string.Concat("Extracting texture ", textureNames[j], " from WAD ", Path.GetFileName(wadFiles[k]), "..."));
+                                textures[j].Image.Save(string.Concat(args[1], "/wads/", Path.GetFileNameWithoutExtension(wadFiles[k]), "/", textureNames[j], ".png"));
+                            }
+
+                        }
+
+                        sl.Close();
+                        wad3l.Close();
                     }
                 }
             }
